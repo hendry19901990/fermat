b36c83f3eac5327ac0c3d6bdb6bd09f4b55f0996
Roberto Requena <rart3001@gmail.com>
2016-07-16 14:58:17 -0400

    Update to use only one instance of Gson

--- P2P/plugin/communication/fermat-p2p-plugin-communication-network-node-bitdubai/src/main/java/com/bitdubai/fermat_p2p_plugin/layer/communications/network/node/developer/bitdubai/version_1/structure/util/PackageDecoder.java
+++ P2P/plugin/communication/fermat-p2p-plugin-communication-network-node-bitdubai/src/main/java/com/bitdubai/fermat_p2p_plugin/layer/communications/network/node/developer/bitdubai/version_1/structure/util/PackageDecoder.java
@@ -7,7 +7,6 @@
 package com.bitdubai.fermat_p2p_plugin.layer.communications.network.node.developer.bitdubai.version_1.structure.util;
 
 import com.bitdubai.fermat_p2p_api.layer.all_definition.communication.commons.data.Package;
-
 import com.bitdubai.fermat_p2p_api.layer.all_definition.communication.commons.util.GsonProvider;
 import com.google.gson.Gson;
 import com.google.gson.JsonParser;
@@ -68,7 +67,7 @@ public class PackageDecoder implements Decoder.Text<Package>{
      */
     @Override
     public void init(EndpointConfig config) {
-        gson = new Gson();
+        gson = GsonProvider.getGson();
         parser = GsonProvider.getJsonParser();
     }
 
