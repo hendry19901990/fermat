d2608d20a79c7f2e032889af32576cc42f4e6a8d
Roberto Requena <rart3001@gmail.com>
2016-07-24 14:46:35 -0400

    add new methods for test

--- P2P/plugin/communication/fermat-p2p-plugin-communication-network-node-bitdubai/src/main/java/com/bitdubai/fermat_p2p_plugin/layer/communications/network/node/developer/bitdubai/version_1/jpa_test/MainRunner.java
+++ P2P/plugin/communication/fermat-p2p-plugin-communication-network-node-bitdubai/src/main/java/com/bitdubai/fermat_p2p_plugin/layer/communications/network/node/developer/bitdubai/version_1/jpa_test/MainRunner.java
@@ -1,13 +1,18 @@
 package com.bitdubai.fermat_p2p_plugin.layer.communications.network.node.developer.bitdubai.version_1.jpa_test;
 
 import com.bitdubai.fermat_api.layer.all_definition.crypto.asymmetric.ECCKeyPair;
+import com.bitdubai.fermat_api.layer.all_definition.network_service.enums.NetworkServiceType;
 import com.bitdubai.fermat_p2p_api.layer.all_definition.communication.commons.enums.ProfileStatus;
+import com.bitdubai.fermat_p2p_api.layer.all_definition.communication.commons.profiles.NetworkServiceProfile;
 import com.bitdubai.fermat_p2p_plugin.layer.communications.network.node.developer.bitdubai.version_1.structure.database.jpa.DatabaseManager;
 import com.bitdubai.fermat_p2p_plugin.layer.communications.network.node.developer.bitdubai.version_1.structure.database.jpa.daos.ClientCheckInDao;
+import com.bitdubai.fermat_p2p_plugin.layer.communications.network.node.developer.bitdubai.version_1.structure.database.jpa.daos.NetworkServiceCheckInDao;
 import com.bitdubai.fermat_p2p_plugin.layer.communications.network.node.developer.bitdubai.version_1.structure.database.jpa.daos.NodeCatalogDao;
 import com.bitdubai.fermat_p2p_plugin.layer.communications.network.node.developer.bitdubai.version_1.structure.database.jpa.entities.Client;
 import com.bitdubai.fermat_p2p_plugin.layer.communications.network.node.developer.bitdubai.version_1.structure.database.jpa.entities.ClientCheckIn;
 import com.bitdubai.fermat_p2p_plugin.layer.communications.network.node.developer.bitdubai.version_1.structure.database.jpa.entities.GeoLocation;
+import com.bitdubai.fermat_p2p_plugin.layer.communications.network.node.developer.bitdubai.version_1.structure.database.jpa.entities.NetworkService;
+import com.bitdubai.fermat_p2p_plugin.layer.communications.network.node.developer.bitdubai.version_1.structure.database.jpa.entities.NetworkServiceCheckIn;
 import com.bitdubai.fermat_p2p_plugin.layer.communications.network.node.developer.bitdubai.version_1.structure.database.jpa.entities.NodeCatalog;
 import com.bitdubai.fermat_p2p_plugin.layer.communications.network.node.developer.bitdubai.version_1.structure.exceptions.CantReadRecordDataBaseException;
 import com.google.common.base.Stopwatch;
@@ -32,6 +37,8 @@ public class MainRunner {
 
             ClientCheckIn clientCheckIn = testClientCheckIn();
 
+            NetworkServiceCheckIn networkServiceCheckIn = testNetworkServiceCheckIn(clientCheckIn);
+
             DatabaseManager.closeDataBase();
 
             System.out.println("TOTAL TEST TOOK: " + timer.stop());
@@ -48,8 +55,8 @@ public class MainRunner {
         System.out.println(" ---------------------------------------------------------------------------------- ");
 
         Stopwatch timer = Stopwatch.createStarted();
-        List<NodeCatalog> nodeCatalogs = new ArrayList<>();
-        NodeCatalogDao nodeCatalogDao = new NodeCatalogDao();
+        List<NodeCatalog> list = new ArrayList<>();
+        NodeCatalogDao dao = new NodeCatalogDao();
 
         ECCKeyPair eccKeyPair = null;
 
@@ -64,23 +71,23 @@ public class MainRunner {
             nodeCatalog.setStatus(ProfileStatus.OFFLINE);
             nodeCatalog.setLocation(new GeoLocation((10.1 + i), (8.9 + i)));
 
-            nodeCatalogs.add(nodeCatalog);
+            list.add(nodeCatalog);
 
         }
 
-        for (NodeCatalog nodeCatalog: nodeCatalogs) {
-            nodeCatalogDao.save(nodeCatalog);
+        for (NodeCatalog item: list) {
+            dao.save(item);
         }
 
         System.out.println("Last id: " + eccKeyPair.getPublicKey());
-        System.out.println("Total nodeCatalog: " + nodeCatalogDao.count());
+        System.out.println("Total entities: " + dao.count());
 
-        NodeCatalog nodeCatalog = nodeCatalogDao.findById(eccKeyPair.getPublicKey());
+        NodeCatalog entity = dao.findById(eccKeyPair.getPublicKey());
 
-        System.out.println("Load entity:" +nodeCatalog);
+        System.out.println("Load entity:" +entity);
         System.out.println("Method testNodeCatalog() took: " + timer.stop());
 
-        return nodeCatalog;
+        return entity;
 
     }
 
@@ -90,40 +97,86 @@ public class MainRunner {
         System.out.println(" ---------------------------------------------------------------------------------- ");
 
         Stopwatch timer = Stopwatch.createStarted();
-        List<ClientCheckIn> clientCheckInList = new ArrayList<>();
-        ClientCheckInDao clientCheckInDao = new ClientCheckInDao();
+        List<ClientCheckIn> list = new ArrayList<>();
+        ClientCheckInDao dao = new ClientCheckInDao();
 
         String id = null;
 
         for (int i = 0; i < 100; i++) {
 
             id = UUID.randomUUID().toString();
-            Client clientProfile = new Client();
-            clientProfile.setDeviceType("device " + i);
-            clientProfile.setId(new ECCKeyPair().getPublicKey());
-            clientProfile.setLocation(new GeoLocation((10.1 + i), (8.9 + i)));
-            clientProfile.setStatus(ProfileStatus.ONLINE);
+            Client profile = new Client();
+            profile.setDeviceType("device " + i);
+            profile.setId(new ECCKeyPair().getPublicKey());
+            profile.setLocation(new GeoLocation((10.1 + i), (8.9 + i)));
+            profile.setStatus(ProfileStatus.ONLINE);
 
             ClientCheckIn clientCheckIn = new ClientCheckIn();
-            clientCheckIn.setClient(clientProfile);
+            clientCheckIn.setClient(profile);
             clientCheckIn.setId(id);
 
-            clientCheckInList.add(clientCheckIn);
+            list.add(clientCheckIn);
 
         }
 
-        for (ClientCheckIn clientCheckIn: clientCheckInList) {
-            clientCheckInDao.save(clientCheckIn);
+        for (ClientCheckIn item: list) {
+            dao.save(item);
         }
 
         System.out.println("Last id: " + id);
-        System.out.println("Total ClientCheckIn: " + clientCheckInDao.count());
+        System.out.println("Total entities: " + dao.count());
 
-        ClientCheckIn clientCheckIn = clientCheckInDao.findById(id);
-        System.out.println("Load entity:" +clientCheckIn);
+        ClientCheckIn entity = dao.findById(id);
+        System.out.println("Load entity:" +entity);
         System.out.println("Method testClientCheckIn() took: " + timer.stop());
 
-        return clientCheckIn;
+        return entity;
+
+    }
+
+
+    public static NetworkServiceCheckIn testNetworkServiceCheckIn(ClientCheckIn clientCheckIn) throws CantReadRecordDataBaseException {
+
+        System.out.println(" ---------------------------------------------------------------------------------- ");
+
+        Stopwatch timer = Stopwatch.createStarted();
+        List<NetworkServiceCheckIn> list = new ArrayList<>();
+        NetworkServiceCheckInDao dao = new NetworkServiceCheckInDao();
+
+        String id = null;
+
+        for (int i = 0; i < 100; i++) {
+
+            id = UUID.randomUUID().toString();
+            NetworkServiceProfile profile = new NetworkServiceProfile();
+            profile.setIdentityPublicKey(new ECCKeyPair().getPublicKey());
+            profile.setLocation(new GeoLocation((10.1 + i), (8.9 + i)));
+            profile.setStatus(ProfileStatus.ONLINE);
+            profile.setClientIdentityPublicKey(clientCheckIn.getClient().getId());
+            profile.setNetworkServiceType(NetworkServiceType.NEGOTIATION_TRANSMISSION);
+
+            NetworkService networkService = new NetworkService(profile);
+
+            NetworkServiceCheckIn networkServiceCheckIn = new NetworkServiceCheckIn();
+            networkServiceCheckIn.setId(id);
+            networkServiceCheckIn.setNetworkService(networkService);
+
+            list.add(networkServiceCheckIn);
+
+        }
+
+        for (NetworkServiceCheckIn item: list) {
+            dao.save(item);
+        }
+
+        System.out.println("Last id: " + id);
+        System.out.println("Total entities: " + dao.count());
+
+        NetworkServiceCheckIn entity = dao.findById(id);
+        System.out.println("Load entity:" +entity);
+        System.out.println("Method testClientCheckIn() took: " + timer.stop());
+
+        return entity;
 
     }
 }
