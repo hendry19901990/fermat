22552f6d1e60d89539a5a9db2d6adb1a68d4f4da
Roberto Requena <rart3001@gmail.com>
2016-07-24 14:01:30 -0400

    Update implementation

--- P2P/plugin/communication/fermat-p2p-plugin-communication-network-node-bitdubai/src/main/java/com/bitdubai/fermat_p2p_plugin/layer/communications/network/node/developer/bitdubai/version_1/jpa_test/MainRunner.java
+++ P2P/plugin/communication/fermat-p2p-plugin-communication-network-node-bitdubai/src/main/java/com/bitdubai/fermat_p2p_plugin/layer/communications/network/node/developer/bitdubai/version_1/jpa_test/MainRunner.java
@@ -3,9 +3,16 @@ package com.bitdubai.fermat_p2p_plugin.layer.communications.network.node.develop
 import com.bitdubai.fermat_api.layer.all_definition.crypto.asymmetric.ECCKeyPair;
 import com.bitdubai.fermat_api.layer.all_definition.location_system.NetworkNodeCommunicationDeviceLocation;
 import com.bitdubai.fermat_p2p_api.layer.all_definition.communication.commons.enums.ProfileStatus;
+import com.bitdubai.fermat_p2p_api.layer.all_definition.communication.commons.profiles.ClientProfile;
+import com.bitdubai.fermat_p2p_api.layer.all_definition.communication.commons.profiles.NetworkServiceProfile;
 import com.bitdubai.fermat_p2p_api.layer.all_definition.communication.commons.profiles.NodeProfile;
 import com.bitdubai.fermat_p2p_plugin.layer.communications.network.node.developer.bitdubai.version_1.structure.database.jpa.DatabaseManager;
+import com.bitdubai.fermat_p2p_plugin.layer.communications.network.node.developer.bitdubai.version_1.structure.database.jpa.daos.ClientCheckInDao;
 import com.bitdubai.fermat_p2p_plugin.layer.communications.network.node.developer.bitdubai.version_1.structure.database.jpa.daos.NodeCatalogDao;
+import com.bitdubai.fermat_p2p_plugin.layer.communications.network.node.developer.bitdubai.version_1.structure.database.jpa.entities.Client;
+import com.bitdubai.fermat_p2p_plugin.layer.communications.network.node.developer.bitdubai.version_1.structure.database.jpa.entities.ClientCheckIn;
+import com.bitdubai.fermat_p2p_plugin.layer.communications.network.node.developer.bitdubai.version_1.structure.database.jpa.entities.GeoLocation;
+import com.bitdubai.fermat_p2p_plugin.layer.communications.network.node.developer.bitdubai.version_1.structure.database.jpa.entities.NetworkService;
 import com.bitdubai.fermat_p2p_plugin.layer.communications.network.node.developer.bitdubai.version_1.structure.database.jpa.entities.NodeCatalog;
 import com.bitdubai.fermat_p2p_plugin.layer.communications.network.node.developer.bitdubai.version_1.structure.exceptions.CantReadRecordDataBaseException;
 import com.google.common.base.Stopwatch;
@@ -13,6 +20,7 @@ import com.google.common.base.Stopwatch;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.UUID;
 
 /**
  * Created by rrequena on 21/07/16.
@@ -27,9 +35,14 @@ public class MainRunner {
 
             testNodeCatalog();
 
-
+            testClientCheckIn();
 
             DatabaseManager.closeDataBase();
+
+
+            ClientProfile clientProfile = new ClientProfile();
+
+            NetworkServiceProfile networkService = new NetworkServiceProfile();
 
         }catch (Exception e){
             e.printStackTrace();
@@ -48,15 +61,15 @@ public class MainRunner {
         for (int i = 0; i < 100; i++) {
 
             eccKeyPair = new ECCKeyPair();
-            NodeProfile nodeProfile = new NodeProfile();
-            nodeProfile.setIdentityPublicKey(eccKeyPair.getPublicKey());
-            nodeProfile.setDefaultPort(8080);
-            nodeProfile.setName("Node_" + i);
-            nodeProfile.setIp("10.1.1." + i);
-            nodeProfile.setStatus(ProfileStatus.OFFLINE);
-            nodeProfile.setLocation(NetworkNodeCommunicationDeviceLocation.getInstance((10.1 + i), (8.9 + i)));
+            NodeCatalog nodeCatalog = new NodeCatalog();
+            nodeCatalog.setId(eccKeyPair.getPublicKey());
+            nodeCatalog.setDefaultPort(8080);
+            nodeCatalog.setName("Node_" + i);
+            nodeCatalog.setIp("10.1.1." + i);
+            nodeCatalog.setStatus(ProfileStatus.OFFLINE);
+            nodeCatalog.setLocation(new GeoLocation((10.1 + i), (8.9 + i)));
 
-            nodeCatalogs.add(new NodeCatalog(nodeProfile));
+            nodeCatalogs.add(nodeCatalog);
 
         }
 
@@ -75,6 +88,51 @@ public class MainRunner {
         }
 
         System.out.println("Method testNodeCatalog() took: " + timer.stop());
+
+    }
+
+
+    public static void testClientCheckIn() throws CantReadRecordDataBaseException {
+
+        Stopwatch timer = Stopwatch.createStarted();
+        List<ClientCheckIn> clientCheckInList = new ArrayList<>();
+        ClientCheckInDao clientCheckInDao = new ClientCheckInDao();
+
+        ECCKeyPair eccKeyPair = null;
+
+        for (int i = 0; i < 100; i++) {
+
+            eccKeyPair = new ECCKeyPair();
+
+            Client clientProfile = new Client();
+            clientProfile.setDeviceType("device " + i);
+            clientProfile.setId(eccKeyPair.getPublicKey());
+            clientProfile.setLocation(new GeoLocation((10.1 + i), (8.9 + i)));
+            clientProfile.setStatus(ProfileStatus.ONLINE);
+
+            ClientCheckIn clientCheckIn = new ClientCheckIn();
+            clientCheckIn.setClientProfile(clientProfile);
+            clientCheckIn.setId(UUID.randomUUID().toString());
+
+            clientCheckInList.add(clientCheckIn);
+
+        }
+
+        for (ClientCheckIn clientCheckIn: clientCheckInList) {
+            clientCheckInDao.save(clientCheckIn);
+        }
+
+        System.out.println("Total nodeCatalog: " + clientCheckInDao.count());
+
+        System.out.println("Load entity:" +clientCheckInDao.findById(eccKeyPair.getPublicKey()));
+
+        // Retrieve all the Point objects from the database:
+        List<ClientCheckIn> results = clientCheckInDao.list();
+        for (ClientCheckIn checkIn : results) {
+            System.out.println(checkIn);
+        }
+
+        System.out.println("Method testClientCheckIn() took: " + timer.stop());
 
     }
 }
