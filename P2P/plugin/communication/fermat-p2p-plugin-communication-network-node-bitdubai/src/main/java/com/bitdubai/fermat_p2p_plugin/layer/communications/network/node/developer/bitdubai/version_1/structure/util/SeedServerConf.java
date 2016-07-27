7f15908acdd79ffa4ed5766dd8dff4feffba069e
franklinmarcano1970 <franklinmarcano1970@gmail.com>
2016-07-27 18:12:23 -0400

    IMPLEMENTATION: fix for test

--- P2P/plugin/communication/fermat-p2p-plugin-communication-network-node-bitdubai/src/main/java/com/bitdubai/fermat_p2p_plugin/layer/communications/network/node/developer/bitdubai/version_1/structure/util/SeedServerConf.java
+++ P2P/plugin/communication/fermat-p2p-plugin-communication-network-node-bitdubai/src/main/java/com/bitdubai/fermat_p2p_plugin/layer/communications/network/node/developer/bitdubai/version_1/structure/util/SeedServerConf.java
@@ -13,7 +13,7 @@ public class SeedServerConf {
     /**
      * Represent the DEFAULT_IP
      */
-    public static final String DEFAULT_IP = "52.32.106.35";
+    public static final String DEFAULT_IP = "192.168.100.110";
 
     /**
      * Represent the DEFAULT_PORT
